f1839b31de9ea98cf28b4c26f1b8770792684f58
Cheung <davcheung@deloitte.nl>
2021-05-07 12:04:02 +0200

    commit

--- src/components/meetups/ContactInfo.js
+++ src/components/meetups/ContactInfo.js
@@ -1,40 +1,48 @@
 import Card from "../ui/Card";
-import { useRef } from 'react';
+import { useRef } from "react";
+import { useState } from "react";
+import Modal from "react-modal";
 
-function ContactInfo (props){
-    const nameInputRef = useRef()
-    const emailInputRef = useRef()
+function ContactInfo(props) {
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    function submitHandler(event) {
-        event.preventDefault();
+  function submitHandler(event) {
+    event.preventDefault();
 
-        const enteredName = nameInputRef.current.value;
-        const enteredEmail = emailInputRef.current.value;
+    const enteredName = nameInputRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
 
-        const personData = {
-            name: enteredName,
-            email: enteredEmail
-        }
+    const personData = {
+      name: enteredName,
+      email: enteredEmail,
+    };
 
-        props.onCreatePerson(personData);
-    }
+    props.onCreatePerson(personData);
+  }
 
-    return <Card>
-        <form onSubmit={submitHandler}>
-            <div>
-                <label>Naam: </label>
-                <input type='text' required id="name" ref={nameInputRef} />
-            </div>
-            <div>
-                <label>Email: </label>
-                <input type='text' required id="email" ref={emailInputRef} />
-            </div>
+  return (
+    <Card>
+      <form onSubmit={submitHandler}>
         <div>
-            <button>Versturen</button>
+          <label>Naam: </label>
+          <input type="text" required id="name" ref={nameInputRef} />
         </div>
-        </form>
+        <div>
+          <label>Email: </label>
+          <input type="text" required id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <button onClick={() => setModalIsOpen(true)}>Versturen</button>
+          <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+            Bedankt voor uw antwoord!
+            <button onClick={() => setModalIsOpen(false)}> sluiten </button>
+          </Modal>
+        </div>
+      </form>
     </Card>
-
+  );
 }
 
 export default ContactInfo;
